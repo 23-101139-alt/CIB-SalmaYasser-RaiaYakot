0827feeb5b346caea325fb63f4cf239d3be82c1d
23-101139-alt <23-101139@students.eui.edu.eg>
2026-02-12 23:59:28 +0200

    sec3-titles-components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,8 @@ import './home.css'
 import Headerr from '../components/layout/header'
 import Herosec from '../components/layout/hero'
 import Navigationlinksec2 from '../components/common/navsec2'
-
+import Title1sec3 from '../components/common/smalltitlesec3'
+import Bigtitlesec3 from '../components/common/bigtitlesec3'
 
 const Home = () => {
     return ( 
@@ -24,6 +25,13 @@ const Home = () => {
                     <nav className='navv-text-sec2'>CIB International</nav>
                     <nav className='navv-text-sec2'>Sustainable Finance</nav>
                     <nav className='navv-text-sec2'>Awards</nav>
+                </nav>
+            </section>
+            <section className='sec3'>
+                <nav className='container-sec3'>
+                    <Title1sec3 />
+                    <Bigtitlesec3 />
+                    
                 </nav>
             </section>
         </main>
