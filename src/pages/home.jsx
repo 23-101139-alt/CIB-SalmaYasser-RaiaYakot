d6abe0d8ed86b9b76046a9da714e46a81ac68560
23-101139-alt <23-101139@students.eui.edu.eg>
2026-02-12 23:18:29 +0200

    sec2-text-navigation-common-components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './home.css'
 import Headerr from '../components/layout/header'
 import Herosec from '../components/layout/hero'
+import Navigationlinksec2 from '../components/common/navsec2'
 
 
 const Home = () => {
@@ -16,7 +17,14 @@ const Home = () => {
                 <Herosec />
             </section>
             <section className='sec2'>
-
+                <nav className='nav-sec2'>
+                    <Navigationlinksec2 />
+                    <nav className='navv-text-sec2'>News</nav>
+                    <nav className='navv-text-sec2'>Blog Articles</nav>
+                    <nav className='navv-text-sec2'>CIB International</nav>
+                    <nav className='navv-text-sec2'>Sustainable Finance</nav>
+                    <nav className='navv-text-sec2'>Awards</nav>
+                </nav>
             </section>
         </main>
         </>
