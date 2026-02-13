6b5655665af86dffec852a9908f3e0d5555d4797
23-101139-alt <23-101139@students.eui.edu.eg>
2026-02-13 19:07:53 +0200

    linking footer navigation text component

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import '../layout/footer.css'
 import logo from '../../assets/logo.png'
+import Footernavs from '../common/footernavs'
 
 const Footer = () => {
     return (  
         <footer className='footer-big-container'>
             <nav className='first-nav-footer'>
                 <nav className='first-box-footer'>
-                    <img src={logo} alt='logo'></img>
+                    <img src={logo} alt='logo' className='logo-footer'></img>
                     <nav className='main-navigations-footer'>
                         <ul className='list-container-footer'>
-
+                            <Footernavs />
+                            <Footernavs />
+                            <Footernavs />
+                            <Footernavs />
+                        </ul>
+                        <ul className='list-container-footer'>
+                            <Footernavs />
+                            <Footernavs />
+                            <Footernavs />
+                            <Footernavs />
+                        </ul>
+                        <ul className='list-container-footer'>
+                            <Footernavs />
+                            <Footernavs />
+                            <Footernavs />
+                            <Footernavs />
                         </ul>
                     </nav>
                 </nav>
