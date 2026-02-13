a0a2b5303c28489342d048ebcf52ed762e7a7f20
23-101139-alt <23-101139@students.eui.edu.eg>
2026-02-13 20:11:01 +0200

    pic-upload-footer-part1

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import '../layout/footer.css'
 import logo from '../../assets/logo.png'
 import Footernavs from '../common/footernavs'
+import pic11 from '../../assets/pic11.png'
+import pic12 from '../../assets/pic12.png'
+import pic13 from '../../assets/pic13.png'
+import pic14 from '../../assets/pic14.png'
+import pic15 from '../../assets/pic15.png'
+import pic16 from '../../assets/pic16.png'
+
 
 const Footer = () => {
     return (  
@@ -29,8 +36,33 @@ const Footer = () => {
                             <Footernavs />
                         </ul>
                     </nav>
+                    <figure className='footer-imgs-container'>
+                         <img src={pic11} alt='googleplay' className='footer-img-googleplay'></img>
+                        <img src={pic12} alt='App store' className='footer-img-appstore'></img>
+                    </figure>
                 </nav>
+
+                <figure className='img-decoration1-footer'>
+                    <img src={pic13} alt='decoration' ></img>
+                     <img src={pic14} alt='decoration' ></img>
+                </figure>
+
+                <figure className='img-decoration-footer-2'>
+                    <img src={pic15} alt='decoration'></img>
+                    <img src={pic16} alt='decoration'></img>
+                </figure>
+
+
+                <nav className='part2-footer'>
+                <ul>
+                    
+                </ul>
             </nav>
+
+
+
+            </nav>
+
         </footer>
     );
 }
